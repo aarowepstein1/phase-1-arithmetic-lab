a4351d349009a757ec53105da98663dd03b330ab
Aarow Epstein <aarowepstein1@gmail.com>
2022-12-15 19:36:38 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1 = 31;
+let num2 = 2;
+let num3 = 4;
+let num4 = 13;
 
-// Write your code here
+let num5 = 8;
+let num6 = 9;
+let num7 = 16;
+let num8 = 20;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random()) + 1;
+
+const mod = num3 % num4;
+
+const max = Math.max(num5, num6, num7, num8);
